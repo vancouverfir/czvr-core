c68f4e8d406c2dc2c8338699abfedcb149056f8a
mikobeako <111254258+mikobeako@users.noreply.github.com>
2022-12-09 13:26:19 -0800

    Trying to get the mailer to work
    hoping it will work as smtp this way

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -10,8 +10,8 @@
 <div class="container" style="margin-top: 20px">
     <h1 class="blue-text font-weight-bold">Privacy Policy</h1>
     <hr>
-    <iframe style="border: none; margin-top: 10px; margin-bottom: 10px; width: 100%; height: 100vh;" src="https://docs.google.com/document/d/e/2PACX-1vTvoAPMt92D0bPq_Euuc-fHNWpGbe3ExuOoi1lcAKLQsjx7pmgR2JdJ8OsC_Gbt1Q/pub?embedded=true"></iframe>
-    If the PDF is not displaying correctly, you can view it directly <a href="https://docs.google.com/document/d/e/2PACX-1vTvoAPMt92D0bPq_Euuc-fHNWpGbe3ExuOoi1lcAKLQsjx7pmgR2JdJ8OsC_Gbt1Q/pub?embedded=true">here.</a><br></br>
+    <iframe style="border: none; margin-top: 10px; margin-bottom: 10px; width: 100%; height: 100vh;" src="https://czvr.ca/storage/privacy.pdf"></iframe>
+    If the PDF is not displaying correctly, you can view it directly <a href="https://czvr.ca/storage/privacy.pdf">here.</a><br></br>
 
 
 @endsection
