2a999f2b12451b51e5473a9e54f444bf20182b5f
Justin Norman <66281890+JustinNorman02@users.noreply.github.com>
2022-12-12 12:48:19 -0500

    Removing More Winnipeg

--- resources/views/feedback/yourfeedback.blade.php
+++ resources/views/feedback/yourfeedback.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
-@section('title', 'Your Feedback - Winnipeg FIR')
+@section('title', 'Your Feedback - Vancouver FIR')
 @section('description', 'View the feedback that the VATSIM community has submitted to us')
 @section('content')
 
 <div class="container py-4">
     <h1 class="font-weight-bold blue-text">Your Feedback</h1>
-    <h5>This is just some of the great feedback we've received from the thousands of VATSIM users that have flown through Winnipeg!</h5>
+    <h5>This is just some of the great feedback we've received from the thousands of VATSIM users that have flown through Vancouver!</h5>
     <hr>
     @foreach($feedback as $f)
         <div class="card">
@@ -16,6 +16,6 @@
         </div>
         <br>
     @endforeach
-    <p>Here at Winnipeg, feedback is something we have always valued. Your suggestions, criticisms and hints make us better controllers! If you haven't yet sent us some feedback about a recent experience you've had on the network, you can do so <a href = "/feedback">HERE</a>.
+    <p>Here at Vancouver, feedback is something we have always valued. Your suggestions, criticisms and hints make us better controllers! If you haven't yet sent us some feedback about a recent experience you've had on the network, you can do so <a href = "/feedback">HERE</a>.
 </div>
 @endsection
