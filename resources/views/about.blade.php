29495234d6a5cfd0cd43b56f219d11196fd7920c
Austin Abbey <aabbey33@gmail.com>
2022-12-13 18:53:38 -0800

    Update about.blade.php
    RIP WPGCore lmao

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container d-flex py-5 flex-column align-items-center justify-content-center">
 <img src="https://cdn.discordapp.com/attachments/800588233570123776/1051930179821391912/Wordmark_Colour.png" class="img-fluid" style="width: 125px;" alt="">
-<h1 class="heading blue-text font-weight-bold display-5">WPGCore</h1>
+<h1 class="heading blue-text font-weight-bold display-5">CZVRCore</h1>
 <h4>Release {{App\Models\Settings\CoreSettings::where('id', 1)->firstOrFail()->release}} ({{App\Models\Settings\CoreSettings::where('id', 1)->firstOrFail()->sys_build}})</h4>
 <br>
 <h5 class="text-center"><a href="https://github.com/vancouverfir/czvr-core">View our GitHub</a></h5>
