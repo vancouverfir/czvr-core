72860813ad1e3f0dc916625b20dd74b281683812
mikobeako <111254258+mikobeako@users.noreply.github.com>
2022-12-16 10:02:37 -0800

    Update mochi.blade.php
    adaptive mochi

--- resources/views/mochi.blade.php
+++ resources/views/mochi.blade.php
@@ -6,7 +6,7 @@
     <h1><strong>Congrats! You've found an Easter Egg.</strong></h1>
         <p>We don't really have anything to give you as a gift, but please feel free to click on Mochi below this text for a free chirping.</p>
     <hr>
-        <img src="https://czvr.ca/storage/files/uploads/1671213201.png" onclick="clickMochi()">
+        <img src="https://czvr.ca/storage/files/uploads/1671213201.png" style="width: 100%; height: auto;" onclick="clickMochi()">
     <br></br>
         <a href="{{route('index')}}" class="blue-text" style="font-size: 1.2em"> <i class="fas fa-arrow-left"></i> Take Me Home (Country Roads)</a>
 </div>
