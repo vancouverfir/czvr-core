6d446a071af82504585606d648fba4b63244c5bf
mikobeako <111254258+mikobeako@users.noreply.github.com>
2022-12-16 13:52:01 -0800

    undo

--- app/Http/Controllers/AtcTraining/TeachersController.php
+++ app/Http/Controllers/AtcTraining/TeachersController.php
@@ -12,11 +12,10 @@ class TeachersController extends Controller
     {
         $teacher = new Teacher;
         $teacher->user_cid = $request->input('newteacher');
-        $teacher->is_twr = $request->input('is_twr');
-        $teacher->is_gnd = $request->input('is_gnd');
-        $teacher->is_radar = $request->input('is_radar');
-        $teacher->is_enroute = $request->input('is_enroute');
-        $teacher->is_instructor = $request->input('is_instructor');
+        $teacher->is_local = $request->has('is_local');
+        $teacher->is_radar = $request->has('is_radar');
+        $teacher->is_enroute = $request->has('is_enroute');
+        $teacher->is_instructor = $request->has('is_instructor');
         $teacher->save();
 
         return redirect()->route('instructors');
