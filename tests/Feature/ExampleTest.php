cda72af21a069a45ed3ec1d7795417ec4faaae11
Justin Norman <66281890+JustinNorman02@users.noreply.github.com>
2023-09-19 20:40:21 -0400

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,7 +13,7 @@ class TaskTest extends TestCase
      */
     public function a_user_can_browse_tasks()
     {
-        $response = $this->get('/info');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
