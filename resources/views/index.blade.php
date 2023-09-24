28984e5918a77a90d697f5b8694eedf6bf2b5111
Justin Norman <66281890+JustinNorman02@users.noreply.github.com>
2023-09-23 20:20:32 -0400

    Updated homepage photos to not go over 1080px in height

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <link rel="stylesheet" type="text/css" href="{{ asset('/css/home.css') }}" />
     <div>
-        <div data-jarallax data-speed="0.2" class="jarallax" style="height: calc(100vh - 59px)">
+        <div data-jarallax data-speed="0.2" class="jarallax" style="height: min(calc(100vh - 59px), 1080px)">
             <div class="mask flex-center flex-column"
                  style="z-index: 1; width: 100%; background-image: url({{$background->url}}); {{$background->css}}">
                 <div class="container" style="padding-bottom: 20em">
