9bea75f685a37811f94e55b7dd754e205f44ff72
Mike Rice <mike.hj.rice@gmail.com>
2025-05-20 19:44:47 -0700

    continued removing Parsedown

--- app/Models/Feedback/ControllerFeedback.php
+++ app/Models/Feedback/ControllerFeedback.php
@@ -3,25 +3,32 @@
 namespace App\Models\Feedback;
 
 use App\Models\Users\User;
+use App\Traits\HasMarkdownFields;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\HtmlString;
-use Parsedown;
 
 class ControllerFeedback extends Model
 {
+    use HasMarkdownFields;
+
     protected $hidden = ['id'];
 
     protected $fillable = [
         'user_id', 'controller_cid', 'position', 'content',
     ];
 
+     /**
+     * Retrieve the list of fields that should be processed as Markdown.
+     * Required for HasMarkdownFields
+     *
+     * @return array An array of field names that are treated as Markdown.
+     */
+    protected function getMarkdownFields(): array
+    {
+        return ['content'];
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function html()
-    {
-        return new HtmlString(app(Parsedown::class)->text($this->content));
     }
 }
