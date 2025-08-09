23c1ce3500979b60e43c4f38692723883e2fd4fe
EncodedWasHere <76215916+EncodedWasHere@users.noreply.github.com>
2025-08-09 18:42:52 +0200

    1

--- database/migrations/2025_03_27_095551_create_student_checklist_items_table.php
+++ database/migrations/2025_03_27_095551_create_student_checklist_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateStudentChecklistItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedInteger('student_id');
             $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
-
             $table->foreignId('checklist_item_id')->constrained('checklist_items')->onDelete('cascade');
             $table->boolean('completed')->default(false);
             $table->timestamps();
